7f8cbb5c96f12b117798f98d82a62ee189e58eb0
adt40 <adt40@case.edu>
2017-10-26 18:00:29 -0400

    started on web scraper

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 
 public class WebScraper {
 
@@ -6,6 +12,29 @@ public class WebScraper {
 	}
 	
 	public String[] getHeroes() {
+		
+		Document allHerosDoc;
+		try {
+			allHerosDoc = Jsoup.connect("http://www.dotabuff.com/heroes/").get();
+			Element heroElement = allHerosDoc.body().child(0).child(7).child(2).child(2).child(1).child(0);
+			int numberOfHeroes = heroElement.childNodeSize() - 1;
+			for (int i = 0; i < numberOfHeroes; i++) {
+				
+			}
+			
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
